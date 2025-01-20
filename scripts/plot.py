1932784da4c455694595fe84c549e27d4bb357bf
Danila Litskevich <danila.litskevich@stud.uni-goettingen.de>
2025-01-20 10:01:11 +0100

    upd: plotting

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     dataset = sys.argv[1]
     data_estimator = PlotResults(dataset)
     data_estimator.plot_data()
+    data_estimator.plot_best_estimations()
--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 from poisson_deconvolution.microscopy.experiment import MicroscopyExperiment
 from scripts.estimation.estimation_results import SplitEstimationsResults
-from scripts.plotting.types import PREFIX
+from scripts.plotting.types import COLORS, PREFIX
 from scripts.plotting.utils import plot_box
 from scripts.read_dataset import read_dataset
 from scripts.path_constants import DATASET_DIR, OUTPUT_DIR
@@ -79,6 +79,9 @@ class PlotResults:
             )
             for delta in self.deltas
         ]
+        self.estimators = list(
+            self.res_best.estimations[self.plt_config.num_atoms[0]][0].data.keys()
+        )
 
     def plot_data(self):
         savepath = os.path.join(self.img_out_path, "data")
@@ -93,6 +96,7 @@ class PlotResults:
                 )
                 path = os.path.join(savepath, name + ".pdf")
                 plt.savefig(path, bbox_inches="tight", dpi=300)
+                plt.close()
 
     def _plot_data(self, is_zoom, init_points=None):
         exp = self.exp
@@ -110,3 +114,44 @@ class PlotResults:
 
         if init_points is not None:
             plt.scatter(init_points[:, 0], init_points[:, 1], c="r", s=1)
+
+    def plot_best_estimations(self):
+        self._plot_estimations(self.res_best)
+
+    def _plot_estimations(self, split_res: SplitEstimationsResults):
+        exp = self.exp
+        x_lim = self.x_lim
+        y_lim = self.y_lim
+        estimations = split_res.denoised
+        color = COLORS[1]
+        n_col = len(self.plt_config.num_atoms)
+        estimators = self.estimators
+        for estimator in estimators:
+            plt.figure(figsize=(5 * n_col, 2 * 5))
+            for j, num_atoms in enumerate(self.plt_config.num_atoms):
+                res = estimations[num_atoms]
+                plt.subplot(2, n_col, j + 1)
+                exp.plot_data("binary")
+                plt.xticks([])
+                plt.yticks([])
+                for e in res:
+                    e.plot(estimator, color=color, s=6)
+                plot_box(x_lim, y_lim)
+                plt.subplot(2, n_col, n_col + j + 1)
+                exp.plot_data("binary")
+                for e in res:
+                    e.plot(estimator, color=color, s=20)
+                plt.xlim(x_lim)
+                plt.ylim(y_lim)
+                plt.xticks([])
+                plt.yticks([])
+
+            plt.savefig(
+                os.path.join(
+                    self.img_out_path,
+                    f"estimations_d{split_res.split.delta}_{estimator.name}.pdf",
+                ),
+                bbox_inches="tight",
+                dpi=300,
+            )
+            plt.close()
