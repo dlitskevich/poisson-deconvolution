c68e363153325370ab6561e360aa5dd1a41fb24d
Danila Litskevich <danila.litskevich@stud.uni-goettingen.de>
2025-01-22 09:23:36 +0100

    fix: root path

--- scripts/dataset/path_constants.py
+++ scripts/dataset/path_constants.py
@@ -2,6 +2,6 @@ import os
 import pathlib
 
 
-ROOT = pathlib.Path(__file__).parent.parent
+ROOT = pathlib.Path(__file__).parent.parent.parent
 DATASET_DIR = os.path.join(ROOT, "datasets")
 OUTPUT_DIR = os.path.join(ROOT, "results")
